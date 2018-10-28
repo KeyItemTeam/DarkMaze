c3b738a49b8be8f123a7821eb76b4ae771e7724a
Laura Suonperä Lozano <43136220+LauraLuna96@users.noreply.github.com>
2018-10-28 19:49:19 +0100

    añadida nueva información

--- src/states/ranking.js
+++ src/states/ranking.js
@@ -13,10 +13,13 @@ create: function() {
         var texto = game.add.text(100, 200, '¡Ha ganado el segundo jugador!', {font: '30px Courier', fill: '#c6f9ac'});
         var texto = game.add.text(100, 250, 'Con una diferencia de ', {font: '13px Courier', fill: '#ffffff'});
         var texto = game.add.text(270, 250, Math.trunc(tj1-tj2), {font: '13px Courier', fill: '#ffffff'});
-        var texto = game.add.text(295, 250, ' respecto al primer jugador. ', {font: '13px Courier', fill: '#ffffff'});
+        var texto = game.add.text(295, 250, ' segundos respecto al primer jugador. ', {font: '13px Courier', fill: '#ffffff'});
         
     } else if (tj1==tj2){
         var texto = game.add.text(100, 200, '¡Empate!', {font: '30px Courier', fill: '#ffffff'});
+        var texto = game.add.text(100, 250, 'Ambos habéis tardado ', {font: '13px Courier', fill: '#ffffff'});
+        var texto = game.add.text(270, 250, Math.trunc(tj1), {font: '13px Courier', fill: '#ffffff'});
+        var texto = game.add.text(295, 250, ' segundos. ', {font: '13px Courier', fill: '#ffffff'});
     } else if (tj1<tj2) {
         var texto = game.add.text(100, 200, '¡Ha ganado el primer jugador!', {font: '30px Courier', fill: '#c6f9ac'});
         var texto = game.add.text(100, 250, 'Con una diferencia de ', {font: '13px Courier', fill: '#ffffff'});
