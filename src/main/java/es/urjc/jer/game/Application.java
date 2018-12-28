26aa31dbf56f87519c032c8d35381285495b0080
Laura Suonperä Lozano <43136220+LauraLuna96@users.noreply.github.com>
2018-12-29 00:31:57 +0100

    Esqueleto Websockets
    añadido esqueleto, de momento no hace nada porque no hay ningún websocket programado

--- src/main/java/es/urjc/jer/game/Application.java
+++ src/main/java/es/urjc/jer/game/Application.java
@@ -2,10 +2,40 @@ package es.urjc.jer.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@SpringBootApplication
+@EnableWebSocket
+public class Application implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(createPlayerHandler(), "/game")	.setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public WebsocketHandler createPlayerHandler() {
+		return new WebsocketHandler();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+
+
+}
+
+/* import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-}
+} */
---
+++ src/main/java/es/urjc/jer/game/WebsocketHandler.java
@@ -0,0 +1,48 @@
+package es.urjc.jer.game;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class WebsocketHandler extends TextWebSocketHandler {
+	
+	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>(); //guarda los websockets que se crean
+	private ObjectMapper mapper = new ObjectMapper(); //hace posible la lectura de objetos json con jackson
+	
+	@Override //crea sesión cuando te conectas
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("New user: " + session.getId());
+		sessions.put(session.getId(), session);
+		
+	}
+	
+	@Override //borra la sesión cuando te desconectas
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("Session closed: " + session.getId());
+		sessions.remove(session.getId());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		synchronized (message) {		}
+		System.out.println("Message received: " + message.getPayload());
+		JsonNode node = mapper.readTree(message.getPayload());
+		
+		switch (node.get("protocolo").asText()) {
+		//un case para cada tipo de mensaje y un default
+		default:
+			System.out.println("ERROR: Mensaje no soportado");
+		}
+		
+	}
+
+}
